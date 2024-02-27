322f356f929047ab1ec4c0b0a9eeaef7ab9bbb64
MarB-T <danmarbet@gmail.com>
2024-02-27 16:38:06 +0300

    1-stdin.js correction

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,13 +1,14 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('readable', () => {
-	const input = process.stdin.read();
+  const input = process.stdin.read();
 
-	if (input !== null) {
-		process.stdout.write(`Your name is: ${name}\n`);
-	}
+  if (input) {
+    name = input.toString().trim();
+    process.stdout.write(`Your name is: ${name}\n`);
+  }
 });
 
 process.stdin.on('end', () => {
-	process.stdout.write('This important software is now closing\n');
+  process.stdout.write('This important software is now closing\n');
 });
