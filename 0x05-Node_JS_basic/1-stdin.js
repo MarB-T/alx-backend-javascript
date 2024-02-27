ab3385d2ff9fad4e11dfc421d1d5bee6e8a2157b
MarB-T <danmarbet@gmail.com>
2024-02-27 15:58:36 +0300

    Correction 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -3,10 +3,7 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('readable', () => {
 	const input = process.stdin.read();
 
-	if (input !== null) {
-		const name = input.toString().trim();
-		process.stdout.write(`Your name is: ${name}\n`);
-	}
+	process.stdout.write(`Your name is: ${input}\n`);
 });
 
 process.stdin.on('end', () => {
